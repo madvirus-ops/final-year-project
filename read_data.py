952c32f1f346f52fc43fa0808d66c80c6e089463
madvirus-ops <edwinayabie1@gmail.com>
2023-10-28 21:23:35 +0100

    fixed df issue

---
+++ read_data.py
@@ -0,0 +1,41 @@
+# import pandas as pd
+
+# data = pd.read_csv('fyp/final_data_csv.csv')
+
+# # Convert 'DOY' to integer type
+# data['DOY'] = data['DOY'].astype(int)
+
+# # Group by the 'DOY' column and calculate the mean for each group along columns
+# daily_average = data.groupby('DOY').mean()
+
+# # Merge the daily averages back to the original data
+# result = pd.merge(data, daily_average, how='left', on='DOY', suffixes=('', '_daily_avg'))
+
+# # Print the result
+# # print(result)
+
+# # Save the result to Excel
+# result.to_excel('result.xlsx', index=False)
+
+
+
+
+# this worked
+import pandas as pd
+
+data = pd.read_csv('fyp/final_data_csv.csv')
+
+# Convert 'DOY' to integer type
+data['DOY'] = data['DOY'].astype(int)
+
+# Group by 'Year' and 'DOY' columns and calculate the mean for each group along columns
+yearly_daily_average = data.groupby(['YEAR', 'DOY']).mean()
+
+# Reset the index to make 'Year' and 'DOY' as regular columns
+yearly_daily_average.reset_index(inplace=True)
+
+# Print the result
+print(yearly_daily_average)
+
+# Save the result to Excel
+yearly_daily_average.to_excel('result2.xlsx', index=False)
