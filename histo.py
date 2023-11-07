d653d82cb89757eb61bd67443566d598bf987280
madvirus-ops <edwinayabie1@gmail.com>
2023-11-07 12:48:06 +0100

    fix

--- histo.py
+++ histo.py
@@ -7,17 +7,7 @@ file_path = "final_data_csv.csv"  # Replace with the actual path to your CSV fil
 data = pd.read_csv(file_path)
 
 # Create the 'plots' directory if it doesn't exist
-os.makedirs("plot_hour", exist_ok=True)
-
-# Exclude the 'YEAR' column if it exists
-# if 'YEAR' in data.columns:
-#     data = data.drop(columns=['YEAR'])
-
-# Convert 'Hour' column to numeric and handle non-numeric values
-data['Hour'] = pd.to_numeric(data['Hour'], errors='coerce').ffill()
-
-# Ensure 'Hour' column has the same length as other columns
-data = data.dropna(subset=['Hour'])
+os.makedirs("histod", exist_ok=True)
 
 # Get unique years in the dataset
 unique_years = data['YEAR'].unique()
@@ -25,27 +15,26 @@ unique_years = data['YEAR'].unique()
 # Create a color map with a unique color for each year
 color_map = plt.cm.get_cmap('tab10', len(unique_years))
 
-# Plot each column against the 'Hour' with a different color for each year
+# Iterate through each column and create a histogram for the number of data points against each year
 for column in data.columns:
     # Replace invalid characters in the column name
     sanitized_column_name = column.replace(" ", "_").replace("/", "_").replace(",", "").replace("(", "").replace(")", "")
-    
-    # Create a new figure for each plot
-    plt.figure()
 
+    plt.figure()
+    
     for i, year in enumerate(unique_years):
         year_data = data[data['YEAR'] == year]
-        plt.plot(year_data['Hour'], year_data[column], label=str(year), color=color_map(i))
+        plt.bar(year, len(year_data), color=color_map(i), label=str(year))
 
-    plt.xlabel('Day of Year (Hour)')
-    plt.ylabel(column)
-    plt.title(f'{column} vs. Hour')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Data Points')
+    plt.title(f'Number of Data Points vs. Year for {column}')
     plt.legend(title='Year', loc='upper right')
 
     # Save the plot with the sanitized column name
-    plt.savefig(f'plot_hour/{sanitized_column_name}_vs_DOY.png')
+    plt.savefig(f'histod/{sanitized_column_name}_data_points_vs_year.png')
 
     plt.close()  # Close the current figure
 
 # Optionally, display a message indicating the completion
-print("Plots saved to the 'plots' directory.")
+print("Histograms saved to the 'plots' directory.")
