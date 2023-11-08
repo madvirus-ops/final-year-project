e1713ef76a44eecd247bd5ebf91b0fd4c6e715b3
madvirus-ops <edwinayabie1@gmail.com>
2023-11-08 12:03:37 +0100

    subplots

--- subplot.py
+++ subplot.py
@@ -2,16 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# Read the CSV file into a Pandas DataFrame
-file_path = "average_daily.xlsx"  # Replace with the actual path to your CSV file
+# Read the Excel file into a Pandas DataFrame
+file_path = "average_daily.xlsx"  # Replace with the actual path to your Excel file
 data = pd.read_excel(file_path)
 
 # Create the 'plots' directory if it doesn't exist
 os.makedirs("plot_hour", exist_ok=True)
-
-# Exclude the 'YEAR' column if it exists
-# if 'YEAR' in data.columns:
-#     data = data.drop(columns=['YEAR'])
 
 # Convert 'Hour' column to numeric and handle non-numeric values
 data['Hour'] = pd.to_numeric(data['Hour'], errors='coerce').ffill()
@@ -25,27 +21,28 @@ unique_years = data['YEAR'].unique()
 # Create a color map with a unique color for each year
 color_map = plt.cm.get_cmap('tab10', len(unique_years))
 
-# Plot each column against the 'Hour' with a different color for each year
+# Iterate through each column and create subplots for each year
 for column in data.columns:
     # Replace invalid characters in the column name
     sanitized_column_name = column.replace(" ", "_").replace("/", "_").replace(",", "").replace("(", "").replace(")", "")
-    
-    # Create a new figure for each plot
-    plt.figure()
+
+    # Create a new figure with subplots
+    fig, axs = plt.subplots(len(unique_years), 1, figsize=(10, 5 * len(unique_years)), sharex=True)
 
     for i, year in enumerate(unique_years):
         year_data = data[data['YEAR'] == year]
-        plt.plot(year_data['DOY'], year_data[column], label=str(year), color=color_map(i))
+        axs[i].plot(year_data['DOY'], year_data[column], label=str(year), color=color_map(i))
+        axs[i].set_ylabel(column)
+        axs[i].legend(title='Year', loc='upper right')
 
-    plt.xlabel('Day of Year (Hour)')
-    plt.ylabel(column)
-    plt.title(f'{column} vs. Hour')
-    plt.legend(title='Year', loc='upper right')
+    axs[-1].set_xlabel('DOY')
+    plt.suptitle(f'{column} vs. DOY', y=0.92)  # Adjust the title position
+    plt.subplots_adjust(hspace=0.5)  # Adjust the vertical space between subplots
 
     # Save the plot with the sanitized column name
-    plt.savefig(f'plot_hour/{sanitized_column_name}_vs_DOY.png')
+    plt.savefig(f'plot_hour/{sanitized_column_name}_vs_Hour_subplots.png')
 
     plt.close()  # Close the current figure
 
 # Optionally, display a message indicating the completion
-print("Plots saved to the 'plots' directory.")
+print("Plots saved to the 'plot_hour' directory.")
